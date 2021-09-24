93e5c309b98e034aee13c75a4b6ffba0c8b76064
amir arab <amir4rab@gmail.com>
2021-09-24 17:35:17 +0330

    primary button feature added

--- src/components/buttons/pButton.jsx
+++ src/components/buttons/pButton.jsx
@@ -1,8 +1,8 @@
 import { pButton, fullWithPButton } from './buttons.module.scss';
 
-const PButton = ({ onClick = null, children, fullWith, disabled })  => {
+const PButton = ({ onClick = null, children, fullWith, disabled, role })  => {
     return (
-        <button className={ fullWith === true ? fullWithPButton : pButton } onClick={ onClick } disabled={disabled}>
+        <button className={ fullWith === true ? fullWithPButton : pButton } onClick={ onClick } disabled={disabled} role={role}>
             { children }
         </button>
     )
