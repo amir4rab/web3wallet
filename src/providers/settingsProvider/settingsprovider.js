7b8edc3d291798093592ff2a28dd61349dc0f9a2
amir arab <amir4rab@gmail.com>
2021-10-07 18:08:06 +0330

    added settings provider

--- src/providers/settingsProvider/settingsprovider.js
+++ src/providers/settingsProvider/settingsprovider.js
@@ -22,12 +22,24 @@ const initializeDatabase = async ( idb ) => {
             ]
         , storeName);
     }
+};
+
+const arrToObj = (arr) => {
+    const settingsObj = {
+        network: null,
+        currency: null
+    };
+    arr.forEach(item => {
+        settingsObj[item.id] = item.value;
+    })
+    return settingsObj;
 }
 
 const SettingsProvider = ({ children }) => {
     const [ idb ] = useState(new Idb());
     const [ isInitialized, setIsInitialized ] = useState(false);
     const [ settingsArr, setSettingsArr ] = useState([]);
+    const [ settingsObj, setSettingsObj ] = useState({});
 
     const init = useCallback( async _ => {
         await idb.init( 'settings', storeName );
@@ -36,6 +48,7 @@ const SettingsProvider = ({ children }) => {
 
         const data = await idb.getAll(storeName);
 
+        setSettingsObj(arrToObj(data));
         setSettingsArr([...data])
         setIsInitialized(true);
     },[ idb ]);
@@ -43,10 +56,11 @@ const SettingsProvider = ({ children }) => {
     const changeSetting = async (newItem) => {
         await idb.put( newItem, storeName );
         setSettingsArr( pervArr => {
-            const filteredArr = pervArr.filter(item => item.id !== newItem.id);
-            filteredArr.push(newItem);
-            return filteredArr;
-        })
+            const newArr = pervArr.filter(item => item.id !== newItem.id);
+            newArr.push(newItem);
+            setSettingsObj(arrToObj(newArr));
+            return newArr;
+        });
     }
 
     useEffect(_ => {
@@ -56,7 +70,8 @@ const SettingsProvider = ({ children }) => {
     const value = {
         settingsArr,
         changeSetting,
-        isInitialized
+        isInitialized,
+        settingsObj
     };
 
     return (
