039bd70abef3ea58737854f3363fae36abec0c3d
amir arab <amir4rab@gmail.com>
2021-10-25 10:44:13 +0330

    added limiter for api routs

--- pages/api/prices/index.js
+++ pages/api/prices/index.js
@@ -1,9 +1,28 @@
 import { getCryptosIds } from "../../../src/utils/global/cryptosData/cryptoData";
 import GetPricesForApi from "../../../src/utils/backend/getPricesFromApi";
+const slowDown = require("express-slow-down");
+
+const speedLimiter = slowDown({
+    windowMs: 60 * 1000, // 1 minute
+    delayAfter: 1, // allow 1 requests per 15 minutes, then...
+    delayMs: 1000 // begin adding 1s of delay per request above 1:
+});
+
+function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result) => {
+            if (result instanceof Error) {
+                return reject(result)
+            }
+            return resolve(result)
+        })
+    })
+}
 
 const getPricesFromApi = new GetPricesForApi(getCryptosIds(),'usd,eur,gbp,chf,dkk');
 
 export default async function handler(req, res) {
+    await runMiddleware(req, res, speedLimiter);
     const response = await getPricesFromApi.getCurrentPrice();
 
     res.status(200).json({ ...response });
--- pages/api/transactions/token.js
+++ pages/api/transactions/token.js
@@ -1,6 +1,25 @@
 import getTransactions from "../../../src/utils/backend/getTransactions";
+const slowDown = require("express-slow-down");
+
+const speedLimiter = slowDown({
+    windowMs: 60 * 1000, // 1 minute
+    delayAfter: 1, // allow 1 requests per 15 minutes, then...
+    delayMs: 1000 // begin adding 1s of delay per request above 1:
+});
+
+function runMiddleware(req, res, fn) {
+    return new Promise((resolve, reject) => {
+        fn(req, res, (result) => {
+            if (result instanceof Error) {
+                return reject(result)
+            }
+            return resolve(result)
+        })
+    })
+}
 
 export default async function handler(req, res) {
+    await runMiddleware(req, res, speedLimiter);
     const {
         wallet,
         network
