0702f3af90b0be849af97e66bfd3407720a3e892
amir arab <amir4rab@gmail.com>
2021-10-26 17:52:23 +0330

    ios pwa splash screen bug fixes

--- pages/_document.js
+++ pages/_document.js
@@ -29,6 +29,7 @@ class MyDocument extends Document {
                     <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
                     <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
                     <link rel='manifest' href='/manifest.json' />
+                    <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#000000' />
                     <link rel='shortcut icon' href='/favicon.svg' />
 
                     <meta name='twitter:card' content='summary' />
@@ -42,15 +43,43 @@ class MyDocument extends Document {
                     <meta property='og:description' content='Simple and Secure web3 wallet' />
                     <meta property='og:site_name' content='Web3 wallet' />
                     <meta property='og:url' content={`https://${ process.env.DOMAIN }`} />
-                    <meta property='og:image' content={`https://${ process.env.DOMAIN }/icons/apple-touch-icon.png`} />
+                    <meta property='og:image' content={`https://${ process.env.DOMAIN }/icons/apple-touch-icon.png`} /> 
 
-                    <link rel='apple-touch-startup-image' href='/images/apple_splash_2048.png' sizes='2048x2732' />
-                    <link rel='apple-touch-startup-image' href='/images/apple_splash_1668.png' sizes='1668x2224' />
-                    <link rel='apple-touch-startup-image' href='/images/apple_splash_1536.png' sizes='1536x2048' />
-                    <link rel='apple-touch-startup-image' href='/images/apple_splash_1125.png' sizes='1125x2436' />
-                    <link rel='apple-touch-startup-image' href='/images/apple_splash_1242.png' sizes='1242x2208' />
-                    <link rel='apple-touch-startup-image' href='/images/apple_splash_750.png' sizes='750x1334' />
-                    <link rel='apple-touch-startup-image' href='/images/apple_splash_640.png' sizes='640x1136' />
+                    <link 
+                        rel="apple-touch-startup-image" 
+                        href="images/launch-640x1136.png" 
+                        media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" 
+                    />
+                    <link 
+                        rel="apple-touch-startup-image" 
+                        href="images/launch-750x1294.png" 
+                        media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" 
+                    />
+                    <link 
+                        rel="apple-touch-startup-image" 
+                        href="images/launch-1242x2148.png" 
+                        media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" 
+                    />
+                    <link 
+                        rel="apple-touch-startup-image" 
+                        href="images/launch-1125x2436.png" 
+                        media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" 
+                    />
+                    <link 
+                        rel="apple-touch-startup-image" 
+                        href="images/launch-1536x2048.png" 
+                        media="(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)" 
+                    />
+                    <link 
+                        rel="apple-touch-startup-image" 
+                        href="images/launch-1668x2224.png" 
+                        media="(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)" 
+                    />
+                    <link 
+                        rel="apple-touch-startup-image" 
+                        href="images/launch-2048x2732.png" 
+                        media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
+                    />
                 </Head>
                 <body>
                     <Main />
