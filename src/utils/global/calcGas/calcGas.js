30c620f4938abffa0d084286ccd1cbdbc8a5a2d7
amir arab <amir4rab@gmail.com>
2021-10-30 10:55:32 +0330

    added gas calculations for token transfer

--- src/utils/global/calcGas/calcGas.js
+++ src/utils/global/calcGas/calcGas.js
@@ -1,11 +1,14 @@
 import BigNumber from "bignumber.js";
+import erc20Abi from '../abi/erc20Abi';
 
-export const calcGas = async ( web3, coinData, ) => {
+export const calcGas = async ( web3, coinData, toAddress, amount ) => {
     let gasPrice;
+    let gas;
     let higherGasPrice;
     let networkFee;
 
     if ( coinData.type === 'native' ) {
+        gas = 2400;
         gasPrice = await web3.eth.getGasPrice();
         higherGasPrice = new BigNumber(gasPrice).multipliedBy(1.25).toString(10).split('.')[0];
         networkFee = new BigNumber(web3.utils.fromWei(higherGasPrice.toString().split('.')[0], 'ether'))
@@ -13,13 +16,16 @@ export const calcGas = async ( web3, coinData, ) => {
             .multipliedBy(24000).toString(10);
     }
     if ( coinData.type === 'token' ) {
+        const erc20Contract = new web3.eth.Contract(erc20Abi, coinData.tokenAddress);
+        gas = await erc20Contract.methods.transfer( toAddress, amount ).estimateGas();
         gasPrice = await web3.eth.getGasPrice();
         higherGasPrice = new BigNumber(gasPrice).multipliedBy(1.25).toString(10).split('.')[0];
         networkFee = new BigNumber(web3.utils.fromWei(higherGasPrice.toString().split('.')[0], 'ether'))
             .multipliedBy(1.25)
-            .multipliedBy(48000).toString(10);
+            .multipliedBy(gas).toString(10);
     }
     return ({
+        gas,
         gasPrice,
         higherGasPrice,
         networkFee,
--- src/utils/global/transaction/tokenTransaction.js
+++ src/utils/global/transaction/tokenTransaction.js
@@ -1,9 +1,9 @@
 import erc20Abi from '../abi/erc20Abi';
 
-export const tokenTransaction = async ( web3, to, gasPrice, value, contractAddress ) => {
+export const tokenTransaction = async ( web3, to, gasPrice, gas, value, contractAddress ) => {
     try{
         const erc20Contract = new web3.eth.Contract(erc20Abi, contractAddress);
-        erc20Contract.options.gas = 48000;
+        erc20Contract.options.gas = gas;
         erc20Contract.options.gasPrice = gasPrice;
         const response = await erc20Contract.methods.transfer(to, value).send({ from: web3.eth.accounts.wallet[0].address });
         return ({
